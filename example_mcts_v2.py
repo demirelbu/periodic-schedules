dac3f73a458a7df9f08cfa1f0e806ff5e768fb06
Burak Demirel <burak.demirel@ericsson.com>
2020-06-09 09:36:38 +0200

    Final version, updated variables and number of iterations.

--- example_mcts_v2.py
+++ example_mcts_v2.py
@@ -6,20 +6,20 @@ from mcts import MCTSAgent
 
 if __name__ == "__main__":
     # Set the random seeds for reproducibility
-    random.seed(40)
+    random.seed(39)
     # Create a set of feedback control systems
-    variables = systems(3, 2, 2, 5, 2, 45)
+    variables = systems(4, 3, 2, 5, 2, 65)
     # Construct a cost function
     costfunc = costfunction(variables)
     # Create an instant for schedule
     schedule = Schedule(
         [],
         costfunc,
-        9,
+        10,
         variables['no_plants'],
         variables['no_channels'])
     # Create a scheduling bot
-    bot = MCTSAgent(1000000, temperature=1.2)
+    bot = MCTSAgent(50000, temperature=1.4)
     # Search for the near-optimal schedule
     cost, sequence = bot.select_action(schedule)
     # Print the near-optimal schedule and the associated cost
