6d4da87161efa0052067459291ef4c53dcb25f44
Burak Demirel <burak.demirel@ericsson.com>
2020-06-17 15:16:23 +0200

    Update it for a sequence of periods.

--- example_mcts.py
+++ example_mcts.py
@@ -11,16 +11,29 @@ if __name__ == "__main__":
     variables = systems(2, 1, 1, 3, 1, 20)
     # Construct a cost function
     costfunc = costfunction(variables)
-    # Create an instant for schedule
-    schedule = Schedule(
-        [],
-        costfunc,
-        10,
-        variables['no_plants'],
-        variables['no_channels'])
-    # Create a scheduling bot
-    bot = MCTSAgent(50000, temperature=1.2)
-    # Search for the near-optimal schedule
-    cost, sequence = bot.select_action(schedule)
-    # Print the near-optimal schedule and the associated cost
-    print("The near-optimal sequence is {} and its associated cost is {:3.4f}.".format(sequence, cost))
+    for period in range(3, 13):  # period length
+        # Create an instant for schedule
+        schedule = Schedule(
+            [],
+            costfunc,
+            period,
+            variables['no_plants'],
+            variables['no_channels'])
+        # Set the number of iterations
+        no_iterations = {
+            "3": 25,
+            "4": 75,
+            "5": 125,
+            "6": 300,
+            "7": 900,
+            "8": 1800,
+            "9": 4000,
+            "10": 6000,
+            "11": 15000,
+            "12": 40000}
+        # Create a scheduling bot
+        bot = MCTSAgent(no_iterations[str(period)], temperature=1.2)
+        # Search for the near-optimal schedule
+        cost, sequence = bot.select_action(schedule)
+        # Print the near-optimal schedule and the associated cost
+        print("Period: {}, Near-optimal cost: {:3.4f}, Sequence: {}.".format(period, cost, sequence))
